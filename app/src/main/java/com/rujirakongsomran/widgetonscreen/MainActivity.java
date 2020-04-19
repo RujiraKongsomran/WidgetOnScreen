44d9fe5a0a84a832d364ec2bb2511adeecb3d634
Rujira Kongsomran <biwberry@outlook.com>
2020-04-19 10:55:47 +0700

    Write function to save content of widget

--- app/src/main/java/com/rujirakongsomran/widgetonscreen/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/widgetonscreen/MainActivity.java
@@ -3,12 +3,43 @@ package com.rujirakongsomran.widgetonscreen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import io.paperdb.Paper;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnSave;
+    EditText etTitle, etContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initInstances();
     }
+
+    private void initInstances() {
+        // Init Paper
+        Paper.init(this);
+
+        etTitle = (EditText) findViewById(R.id.etTitle);
+        etContent = (EditText) findViewById(R.id.etContent);
+        btnSave = (Button) findViewById(R.id.btnSave);
+
+
+        btnSave.setOnClickListener(btnSaveListener);
+
+    }
+
+    // Listener
+    View.OnClickListener btnSaveListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Paper.book().write("title", etTitle.getText().toString());
+            Paper.book().write("description", etContent.getText().toString());
+        }
+    };
 }
